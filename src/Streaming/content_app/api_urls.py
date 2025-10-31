05910641bc9d3b0b3de17b130bd751cb4f707225
davi santos <davicamaradossantos@outlook.com>
2025-10-31 10:53:55 -0300

    contexto 2

--- src/Streaming/content_app/api_urls.py
+++ src/Streaming/content_app/api_urls.py
@@ -1,12 +1,12 @@
 # myapp/api_urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from Streaming.content_app.api import ProdutoViewSet, ProdutosBaratosAPIView
+from .api import  VagaMonitoriaViewSet, VagaMonitoriaAPIView
 
 router = DefaultRouter()
-router.register(r'produtos', ProdutoViewSet, basename='produto')
+router.register(r'vagas',  VagaMonitoriaViewSet, basename='vaga-monitoria')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('produtos/baratos/', ProdutosBaratosAPIView.as_view(), name='produtos-baratos'),
+    path('produtos/baratos/', VagaMonitoriaAPIView(), name='produtos-baratos'),
 ]
